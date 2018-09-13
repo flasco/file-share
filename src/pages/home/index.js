f2d679bdc4d4717dbf6477af828a85e637b587c5
czq <chenzhengqiang@youzan.com>
2018-09-13 21:05:08 +0800

    fix: 调整目录结构

---
+++ src/pages/home/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import { Button } from 'antd';
+
+import MainLayout from 'components/main-layout';
+
+class Home extends React.Component {
+  test = () => {
+    // console.log('test');
+  }
+
+  render() {
+    return (
+      <MainLayout>
+        <div>Hello</div>
+        <Link to={'/test'}>测试</Link>
+        <a href={'/'}>jmp</a>
+        <Button onClick={this.test}>测试</Button>
+      </MainLayout>
+    );
+  }
+}
+
+function select(state) {
+  console.log(state);
+  return {};
+}
+
+export default connect(select)(Home);
